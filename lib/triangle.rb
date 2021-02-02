e4fb0f3cba7c3c145369c2c0e411a95d9138347e
Reid <rlj0713@gmail.com>
2021-02-01 21:34:52 -0500

    commit tree c5793ebf602c116f95905cc5de5c6dd741528677

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def legal?
+    if length1 <= 0 || length2 <= 0 || length3 <= 0
+      false
+    elsif (length1 + length2) <= length3 || (length2 + length3) <= length1 || (length3 + length1) <= length2
+      false
+    else
+      true
+    end
+  end
+  
+  def kind
+    if self.legal? == false
+      raise TriangleError
+    elsif length1 == length2 && length2 == length3
+      :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      :isosceles
+    else
+      :scalene
+    end
+  
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "This is not a legal triangle."
+    end   
+  end
+  
 end
